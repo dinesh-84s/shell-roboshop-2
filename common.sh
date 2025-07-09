5524c32afc41204ee66fcda9ac03b309b444bf40
dineshkonda <dineshkonda3239@gmail.com>
2025-07-10 00:17:43 +0530

    hello-world-started

--- common.sh
+++ common.sh
@@ -50,6 +50,16 @@ nodejs_setup(){
     VALIDATE $? "Installing Dependencies"
 }
 
+maven_setup(){
+    dnf install maven -y &>>$LOG_FILE
+    VALIDATE $? "Installing maven and java"
+
+    mvn clean package &>>$LOG_FILE
+    VALIDATE $? "Packaging the shipping application"
+
+    mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
+    VALIDATE $? "moving and renaming jar file"
+}
 systemd_setup(){
     cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
     VALIDATE $? "Copying $app_name service"
--- shipping.sh
+++ shipping.sh
@@ -1,83 +1,15 @@
 #!/bin/bash
 
-START_TIME=$(date +%S)
-USERID=$(id -u)
-R="/e[31m"
-G="/e[32m"
-Y="/e[33m"
-N="/e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+souce ./common.sh
+app_name=shipping
 
-mkdir -p $LOGS_FOLDER
-echo "statred and execute the skript at with: $(date)" | tee -a $LOG_FILE
-
-#check users has root previlages or not 
-if [ $USERID -ne 0 ]
-then
-    echo -e $R "ERROR:: please run the script with using root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto to 127
-else 
-    echo "you are running with the root access" | tee -a $LOG_FILE
-fi
-
+check_root
 echo "please enter root password to setup"
 read -s MYSQL_ROOT_PASSWORD
 
-#vaildate functions takes input as exit status what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo "Installing $2 is... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo "Installing $2 is... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-fi
-}
-
-
-dnf install maven -y &>>$LOG_FILE
-VALIDATE $? "Installing maven and java"
-
-id roboshop  &>>$LOG_FILE
-if [ $? -ne 0 ]
-then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE 
-    VALIDATE $? "Creating roboshop system user"
-
-else 
-    echo -e "System user roboshop already created... $Y Skipping $N"
-fi
-
-mkdir -p /app 
-VALIDATE $? "Creating app directory"
-
-curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading shipping"
-
-rm -rf /app/*
-cd /app
-unzip /tmp/shipping.zip &>>$LOG_FILE
-VALIDATE $? "Unzipping the shipping"
-
-mvn clean package &>>$LOG_FILE
-VALIDATE $? "Packaging the shipping application"
-
-mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
-VALIDATE $? "moving and renaming jar file"
-
-cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
-
-systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "Deamon Reload"
-
-systemctl enable shipping  &>>$LOG_FILE
-VALIDATE $? "enabling shipping"
-
-systemctl start shipping  &>>$LOG_FILE
-VALIDATE $? "start shipping"
+app_setup
+maven_setup
+systemd_setup
 
 dnf install mysql -y  &>>$LOG_FILE
 VALIDATE $? "install mysql"
@@ -97,7 +29,4 @@ fi
 systemctl restart shipping  &>>$LOG_FILE
 VALIDATE $? "restart shipping"
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+print_time
